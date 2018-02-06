a625ca138df499487680e592ea2faf668f0b439d
Zhenyu Lin <zhenyu.lyn@gmail.com>
2018-02-06 17:50:09 +0000

    update test
    remove deps on network
     🐿 v2.7.0

--- src/__tests__/error-formatter.js
+++ src/__tests__/error-formatter.js
@@ -1,75 +1,53 @@
-import fetch from 'node-fetch';
-import nock from 'nock';
+import { Response, Headers, FetchError } from 'node-fetch';
 import {
 	formatFetchResponseError,
 	formatFetchNetworkError,
 } from '../error-formatter';
 
-// TODO: mock for fetch here
 describe('formatFetchResponseError', () => {
 	it('reports wrong response being threw to catch if response is ok', async () => {
-		nock('https://somehost.com')
-			.get('/posts')
-			.reply(200);
-		try {
-			const response = await fetch('https://somehost.com/posts');
-			throw response;
-		} catch (e) {
-			const formatted = await formatFetchResponseError(e);
-			expect(formatted).toMatchSnapshot();
-		}
+		const headers = new Headers();
+		headers.append('content-type', 'text/plain; charset=utf-8');
+		const response = new Response('', { status: 200, headers });
+		const formatted = await formatFetchResponseError(response);
+		expect(formatted).toMatchSnapshot();
 	});
 
 	it('format error in text/html contentType correctly', async () => {
-		try {
-			const response = await fetch('http://www.google.com/404');
-			if (!response.ok) {
-				throw response;
-			}
-		} catch (e) {
-			const formatted = await formatFetchResponseError(e);
-			expect(formatted).toMatchSnapshot();
-		}
+		const headers = new Headers();
+		headers.append('content-type', 'text/html; charset=utf-8');
+		const e = new Response('<html></html>', { status: 404, headers });
+		const formatted = await formatFetchResponseError(e);
+		expect(formatted).toMatchSnapshot();
 	});
 
 	it('format error in text/plain contentType correctly', async () => {
-		try {
-			const response = await fetch('https://httpstat.us/403');
-			if (!response.ok) {
-				throw response;
-			}
-		} catch (e) {
-			const formatted = await formatFetchResponseError(e);
-			expect(formatted).toMatchSnapshot();
-		}
+		const headers = new Headers();
+		headers.append('content-type', 'text/plain; charset=utf-8');
+		const e = new Response('403 Forbidden', { status: 403, headers });
+		const formatted = await formatFetchResponseError(e);
+		expect(formatted).toMatchSnapshot();
 	});
 
 	it('format error in application/json contentType correctly', async () => {
-		try {
-			const response = await fetch('https://api.github.com', {
-				method: 'POST',
-			});
-			if (!response.ok) {
-				throw response;
-			}
-		} catch (e) {
-			const formatted = await formatFetchResponseError(e);
-			expect(formatted).toMatchObject({
-				contentType: 'application/json; charset=utf-8',
-			});
-			expect(formatted.message).toHaveProperty('documentation_url');
-			expect(formatted.message).toHaveProperty('message');
-		}
+		const headers = new Headers();
+		headers.append('content-type', 'application/json; charset=utf-8');
+		const e = new Response(
+			JSON.stringify({ message: 'some message', document: 'some url' }),
+			{ status: 404, headers },
+		);
+		const formatted = await formatFetchResponseError(e);
+		expect(formatted).toMatchSnapshot();
 	});
 });
 
 describe('formatFetchNetworkError', () => {
 	it('format network error correctly', async () => {
-		try {
-			await fetch('https://mock.com');
-		} catch (e) {
-			const formatted = await formatFetchNetworkError(e);
-			expect(formatted).toMatchSnapshot();
-		}
+		const e = new FetchError(
+			'request to https://mock.com/ failed, reason: unable to verify the first certificate',
+		);
+		e.code = 'UNABLE_TO_VERIFY_LEAF_SIGNATURE';
+		const formatted = await formatFetchNetworkError(e);
+		expect(formatted).toMatchSnapshot();
 	});
 });
--- src/__tests__/failure.js
+++ src/__tests__/failure.js
@@ -1,5 +1,5 @@
 import logger from '@financial-times/n-logger';
-import fetch from 'node-fetch';
+import { Response, Headers, FetchError } from 'node-fetch';
 import failureLogger from '../failure';
 
 jest.mock('@financial-times/n-logger');
@@ -20,38 +20,34 @@ describe('failureLogger', () => {
 	});
 
 	it('log fetch response error correctly based on status', async () => {
-		try {
-			const response = await fetch('https://httpstat.us/403');
-			if (!response.ok) {
-				throw response;
-			}
-		} catch (e) {
-			await failureLogger()(e);
-			expect(logger.warn.mock.calls).toHaveLength(1);
-			expect(logger.warn.mock.calls[0][0]).toMatchSnapshot();
-		}
+		const headers = new Headers();
+		headers.append('content-type', 'text/plain; charset=utf-8');
+		const errorResponse403 = new Response('403 Forbidden', {
+			status: 403,
+			headers,
+		});
+		await failureLogger()(errorResponse403);
+		expect(logger.warn.mock.calls).toHaveLength(1);
+		expect(logger.warn.mock.calls[0][0]).toMatchSnapshot();
 
-		try {
-			const response = await fetch('https://httpstat.us/500');
-			if (!response.ok) {
-				throw response;
-			}
-		} catch (e) {
-			await failureLogger()(e);
-			expect(logger.warn.mock.calls).toHaveLength(1);
-			expect(logger.error.mock.calls).toHaveLength(1);
-			expect(logger.error.mock.calls[0][0]).toMatchSnapshot();
-		}
+		const errorResponse500 = new Response('500 Internal Server Error', {
+			status: 500,
+			headers,
+		});
+		await failureLogger()(errorResponse500);
+		expect(logger.warn.mock.calls).toHaveLength(1);
+		expect(logger.error.mock.calls).toHaveLength(1);
+		expect(logger.error.mock.calls[0][0]).toMatchSnapshot();
 	});
 
 	it('log fetch network error correctly', async () => {
-		try {
-			await fetch('https://unknown');
-		} catch (e) {
-			await failureLogger()(e);
-			expect(logger.error.mock.calls).toHaveLength(1);
-			expect(logger.error.mock.calls[0][0]).toMatchSnapshot();
-		}
+		const e = new FetchError(
+			'request to https://unknown/ failed, reason: getaddrinfo ENOTFOUND unknown unknown:443',
+		);
+		e.code = 'ENOTFOUND';
+		await failureLogger()(e);
+		expect(logger.error.mock.calls).toHaveLength(1);
+		expect(logger.error.mock.calls[0][0]).toMatchSnapshot();
 	});
 
 	it('log node system error correctly', async () => {
