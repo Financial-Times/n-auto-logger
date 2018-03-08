bf7a8bd31b6756c0ada043281323e6ba15695bfd
Zhenyu Lin <zhenyu.lyn@gmail.com>
2018-03-08 12:07:13 +0000

    minor setup update  🐿 v2.7.0

--- secret-squirrel.js
+++ secret-squirrel.js
@@ -1,13 +1,6 @@
 module.exports = {
 	files: {
-		allow: [
-			'.gitattributes',
-			'src/__tests__/__snapshots__/action.js.snap',
-			'src/__tests__/__snapshots__/failure.js.snap',
-			'src/__tests__/__snapshots__/operation.js.snap',
-			'src/__tests__/__snapshots__/utils.js.snap',
-			'yarn.lock',
-		],
+		allow: ['.gitattributes', 'yarn.lock', '.*\\.(snap)'],
 		allowOverrides: [],
 	},
 	strings: {
