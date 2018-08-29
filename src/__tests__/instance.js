3214d89a139fce19db136e8f53c5577ea4365350
Zhenyu Lin <zhenyu.lyn@gmail.com>
2018-08-29 12:24:00 +0100

    instance test update  🐿 v2.8.0

--- src/__tests__/instance.js
+++ src/__tests__/instance.js
@@ -1,11 +1,7 @@
 import nLogger from '@financial-times/n-logger';
-import winston from 'winston';
-import MaskLogger from '@financial-times/n-mask-logger';
 
 import { setupLoggerInstance, getLoggerInstance } from '../instance';
 import createEventLogger from '../event-logger';
-
-jest.mock('winston');
 
 describe('instance', () => {
 	it('default to n-logger', () => {
@@ -13,36 +9,21 @@ describe('instance', () => {
 		expect(logger).toBe(nLogger);
 	});
 
-	it('can be set to winston and log properly', async () => {
-		setupLoggerInstance(winston);
+	it('can be set to other instance and log properly', async () => {
+		const alternative = {
+			info: jest.fn(),
+			warn: jest.fn(),
+			error: jest.fn(),
+		};
+		setupLoggerInstance(alternative);
 		const logger = getLoggerInstance();
-		expect(logger).toBe(winston);
+		expect(logger).toBe(alternative);
 		const event = createEventLogger({ event: 'USER-SIGNUP' });
 		await event.start();
 		await event.success();
 		await event.failure({ message: 'user with the email already exists' });
-		expect(logger.info.mock.calls).toMatchSnapshot();
-		expect(logger.warn.mock.calls).toMatchSnapshot();
-		expect(logger.error.mock.calls).toMatchSnapshot();
-	});
-
-	it('can be set to MaskLogger and log properly', async () => {
-		const maskLogger = new MaskLogger(['email', 'password']);
-		setupLoggerInstance(maskLogger);
-		const logger = getLoggerInstance();
-		expect(logger).toBe(maskLogger);
-		jest.spyOn(maskLogger, 'info');
-		jest.spyOn(maskLogger, 'warn');
-		jest.spyOn(maskLogger, 'error');
-		const event = createEventLogger({
-			event: 'USER-LOGIN',
-			password: 'dummy-password',
-		});
-		await event.start();
-		await event.success();
-		await event.failure({ message: 'user with the email already exists' });
-		expect(maskLogger.info.mock.calls).toMatchSnapshot();
-		expect(maskLogger.warn.mock.calls).toMatchSnapshot();
-		expect(maskLogger.error.mock.calls).toMatchSnapshot();
+		expect(alternative.info.mock.calls).toMatchSnapshot();
+		expect(alternative.warn.mock.calls).toMatchSnapshot();
+		expect(alternative.error.mock.calls).toMatchSnapshot();
 	});
 });
